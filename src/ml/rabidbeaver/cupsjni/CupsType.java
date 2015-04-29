66abde6badd7d8807304ad75ce3e5e145642c2a4
lbdroid <definesinsanity@gmail.com>
2015-04-29 11:09:06 -0400

    Add the actual data structures

---
+++ src/ml/rabidbeaver/cupsjni/CupsType.java
@@ -0,0 +1,16 @@
+package ml.rabidbeaver.cupsjni;
+
+public enum CupsType {
+    KEYWORD("keyword"), ENUM("enum"), BOOLEAN("boolean"), 
+    INTEGER("integer"), SETOFRANGEOFINTEGER("setOfRangeOfInteger");
+   
+   private final String stringValue;
+   
+   private CupsType(final String s) { 
+       stringValue = s; 
+   }
+   
+   public String toString() {
+       return stringValue;
+   }
+}
---
+++ src/ml/rabidbeaver/cupsjni/PpdSectionList.java
@@ -0,0 +1,31 @@
+package ml.rabidbeaver.cupsjni;
+
+import java.util.ArrayList;
+
+public class PpdSectionList extends ArrayList<PpdItemList>{
+    String name;
+    String text;
+    
+    public String getName(){
+        return name;
+    }
+    
+    public String getGroupText(){
+        return text;
+    }
+    
+    @Override
+    public String toString(){
+        return text;
+    }
+    
+    public PpdSectionList deepClone(){
+        PpdSectionList sectionList = new PpdSectionList();
+        sectionList.name = name;
+        sectionList.text = text;
+        for (PpdItemList itemList: this){
+            sectionList.add(itemList.deepClone());
+        }
+        return sectionList;
+   }
+}
