99f2d74721a65b3e8d0fec1fef12c714b1b9a859
lbdroid <definesinsanity@gmail.com>
2015-04-29 14:01:40 -0400

    A fun experiment

--- jni/cups_shim/Android.mk
+++ jni/cups_shim/Android.mk
@@ -2,8 +2,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ECHO_RESULT := $(shell ($(LOCAL_PATH)/jnaerator.sh $(LOCAL_PATH)))
 LOCAL_MODULE    := cups_shim
-LOCAL_SRC_FILES := shim.c
+#LOCAL_SRC_FILES := shim.c
 LOCAL_SHARED_LIBRARIES := cups
 
 include $(BUILD_SHARED_LIBRARY)
---
+++ jni/cups_shim/jnaerator.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+oldpath=`pwd`
+cd $1
+java -jar ../../libs/jnaerator-0.13-20150328.111636-4-shaded.jar -library ml.rabidbeaver.cupsjni ../cups-2.0.2/cups/cups.h -o ../../libs/ -v -runtime JNAerator -mode StandaloneJar
+cd $oldpath
